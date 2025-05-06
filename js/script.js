a92a67bb888a3293ab5e9ecf61e8698ca6761486
KT0412 <keitake1207@gmail.com>
2025-05-06 22:04:46 +0900

    サイトレイアウトやアニメーション諸々

--- js/script.js
+++ js/script.js
@@ -1,35 +1,30 @@
-function showRestTime() {
-    const now = new Date();
-    const goal = new Date(2025, 5, 1)
-
-    const restMillisecond = goal.getTime() - now.getTime();
-    const day = Math.floor(restMillisecond / 1000 / 60 / 60 / 24);
-    const hour = Math.floor(restMillisecond / 1000 / 60 / 60) % 24;
-    const minute = Math.floor(restMillisecond / 1000 / 60) % 60;
-    const second = Math.floor(restMillisecond / 1000) % 60;
-
-    document.getElementById('day').textContent = day;
-    document.getElementById('hour').textContent = hour;
-    document.getElementById('minute').textContent = String(minute).padStart(2, '0');
-    document.getElementById('second').textContent = String(second).padStart(2, 0);
-
-}
-setInterval(showRestTime, 1000);
-
-
 const mobileNav = $('.mobile-nav');
 const pcNav = $('#pc-header');
 
 const mobileHeight = mobileNav.outerHeight();
-const pcHeight = mobileNav.outerHeight();
+const pcHeight = pcNav.outerHeight();
 
 let beforeScrollTop = 0;
+
+
 $(window).scroll(function () {
+    if($(".mobile-nav").css("display") === "flex"){
     const scrollTop = $(this).scrollTop();
-    if ((scrollTop > beforeScrollTop) && (scrollTop > headerHeight)) {
+    if ((scrollTop > beforeScrollTop) && (scrollTop > mobileHeight)) {
         mobileNav.addClass('js_hide');
     } else {
         mobileNav.removeClass('js_hide');
     }
     beforeScrollTop = scrollTop
+    }else if($("#pc-header").css("display") === "flex"){
+        const scrollTop = $(this).scrollTop();
+        if ((scrollTop > beforeScrollTop) && (scrollTop > pcHeight)) {
+            pcNav.addClass('js_hide');
+        } else {
+            pcNav.removeClass('js_hide');
+        }
+        beforeScrollTop = scrollTop
+    }
+
+
 })
